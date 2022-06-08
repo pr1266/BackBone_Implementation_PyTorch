2aea06866207f9d7ef4f933d0fda0708df56a3cd
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-08 13:10:05 +0430

    'leNet_layers'

--- LeNet/leNet.py
+++ LeNet/leNet.py
@@ -15,4 +15,34 @@ class leNet(nn.Module):
 
     def __init__(self):
         super(leNet, self).__init__()
-        
+        self.relu = nn.ReLU()
+        self.pool = nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.conv1 = nn.Conv2d(
+            in_channels=1, 
+            out_channels=6, 
+            kernel_size=(5, 5), 
+            stride=(1, 1), 
+            padding=(0, 0)
+        )        
+        self.conv2 = nn.Conv2d(
+            in_channels=6, 
+            out_channels=16, 
+            kernel_size=(5, 5), 
+            stride=(1, 1), 
+            padding=(0, 0)
+        )
+        self.conv3 = nn.Conv2d(
+            in_channels=16, 
+            out_channels=120, 
+            kernel_size=(5, 5), 
+            stride=(1, 1), 
+            padding=(0, 0)
+        )
+        self.linear1 = nn.Linear(
+            in_features=120, 
+            out_features=84
+        )
+        self.linear2 = nn.Linear(
+            in_features=84,
+            out_features=10
+        )
