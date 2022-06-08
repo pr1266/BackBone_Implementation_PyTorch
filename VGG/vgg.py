c8baa944b1447370462ba07a6ccd5a0eca773be3
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-08 13:49:47 +0430

    'vgg_create'

--- VGG/vgg.py
+++ VGG/vgg.py
@@ -9,3 +9,21 @@ VGG_types = {
     "VGG16": [64, 64, "M", 128, 128, "M", 256, 256, 256, "M", 512, 512, 512, "M", 512, 512, 512, "M",],
     "VGG19": [ 64, 64, "M", 128, 128, "M", 256, 256, 256, 256, "M", 512, 512, 512, 512, "M", 512, 512, 512, 512, "M",],
 }
+
+class VGG(nn.Module):
+
+    def __init__(self, type='VGG16'):
+        self.num_classes = 1000
+        self.in_channels = 3
+        self.type = type
+        self.conv_layers = self.create_conv_layers(VGG_types[self.type])
+
+        self.fcs = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, self.num_classes)
+        )
