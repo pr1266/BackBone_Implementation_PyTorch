19a28a2b98394d4c811756f9b5cd1d19c00e2ff9
pourya <pourya_1999@yahoo.com>
2022-06-09 00:37:30 +0430

    'res_comment'

--- ResNet/resNet.py
+++ ResNet/resNet.py
@@ -11,19 +11,36 @@ import torch.nn as nn
 #! layer haro ke mohasebe kardim, ba natije conputation
 #! 2 laye baadi jam mikonim
 #! intori yademoon mimoone chi ghablan extract kardim
-
+#! migim agha! harchi tedad layer ha bishtar bashan
+#! ma feature haye high level tari ro mitoonim az tasvir
+#! extract konim
+#! strategy chie? mige agha man miam ye residual block tarif mikonam
+#! x ro voroodi midim, ye seri conv operation roosh anjam mishe
+#! dar nahayat ye F(x) be dast miad, hala ma baraye har block,
+#! miaim output ro F(x) + x dar nazar migirim
+#! tavajoh shavad ke agar size F(x) va x barabar nabashe,
+#! bayad down_sample konim
 class Block(nn.Module):
 
-    def __init__(self, in_channels, out_channels, identity_downsample=None,stride=1):
+    def __init__(self, in_channels, intermediate_channels, identity_downsample=None,stride=1):
         super(Block, self).__init__()
         self.expansion = 4
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
-        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv1 = nn.Conv2d(in_channels, intermediate_channels, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(intermediate_channels)
         self.conv2 = nn.Conv2d(
             intermediate_channels,
             intermediate_channels,
             kernel_size=3,
             stride=stride,
             padding=1,
+            bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(intermediate_channels)
+        self.conv3 = nn.Conv2d(
+            intermediate_channels,
+            intermediate_channels * self.expansion,
+            kernel_size=1,
+            stride=1,
+            padding=0,
             bias=False
         )
