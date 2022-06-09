cdb9f729e2e7acb70a8feda01ba8d2b386e87d8a
pourya <pourya_1999@yahoo.com>
2022-06-10 01:09:09 +0430

    'inverted_res'

--- EfficientNet/efficientNet.py
+++ EfficientNet/efficientNet.py
@@ -40,7 +40,7 @@ class CnnBlock(nn.Module):
     #! age be andaze in_channels bashe, mishe DepthWise conv
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, groups=1):
         super(CnnBlock, self).__init__()
-        self.cnn = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, groups=groups)
+        self.cnn = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, groups=groups, bias=False)
         self.bn = nn.BatchNorm2d(out_channels)
         self.silu = nn.SiLU()
 
@@ -64,8 +64,42 @@ class SqueezeExcitation(nn.Module):
 
 class InvertdResidualBlock(nn.Module):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, expand_ratio, reduction=4, survival_prob=0.8):
+        super(InvertdResidualBlock, self).__init__()
+        self.survival_prob = survival_prob
+        self.use_residual = in_channels == out_channels and stride == 1
+        hidden_dim = in_channels * expand_ratio
+        self.expand = in_channels != hidden_dim
+        reduced_dim = int(in_channels / reduction)
+
+        if self.expand:
+            self.expand_conv = CnnBlock(
+                in_channels, hidden_dim, kernel_size=3, stride=1, padding=1,
+            )
+
+        self.conv = nn.Sequential(
+            CnnBlock(
+                hidden_dim, hidden_dim, kernel_size, stride, padding, groups=hidden_dim,
+            ),
+            SqueezeExcitation(hidden_dim, reduced_dim),
+            nn.Conv2d(hidden_dim, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+        )
+
+    def stochastic_depth(self, x):
+
+        if not self.training:
+            return x
+        binary_tensor = torch.rand(x.shape[0], 1, 1, 1, device=x.device) < self.survival_prob
+        return torch.div(x, self.survival_prob) * binary_tensor
+
+    def forward(self, inputs):
+        x = self.expand_conv(inputs) if self.expand else inputs
+
+        if self.use_residual:
+            return self.stochastic_depth(self.conv(x)) + inputs
+        else:
+            return self.conv(x)
 
 class EfficientNet(nn.Module):
 
