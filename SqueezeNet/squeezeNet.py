959143ad4af65098b21535dde75de11ac858088f
pourya <pourya_1999@yahoo.com>
2022-06-14 16:24:31 +0430

    'sqz_init'

---
+++ SqueezeNet/squeezeNet.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+
+#! too paper harf jalebi zade
+#! oomade mige accuracy AlexNet ro midam
+#! vali ba parameter haye kamtar ve size kamtar az
+#! 0.5 MB!!!! AlexNet 250 MB e!!!! jaleb shod
