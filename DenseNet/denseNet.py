037d639816aad7bd3f499eb2cc446ae4d05b1bcb
pourya <pourya_1999@yahoo.com>
2022-06-19 16:36:47 +0430

    'DenseNet'

---
+++ DenseNet/denseNet.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class DenseNet(nn.Module):
+
+    def __init__(self) -> None:
+        super(DenseNet, self).__init__()
+    
+    def forward(self, x):
+        pass
+
+
+def Test():
+    pass
+
+if __name__ == '__main__':
+    Test()
