61561237e7bcaab5214940b3e1a7261519d94659
pourya <pourya_1999@yahoo.com>
2022-06-22 14:35:17 +0430

    'dense_block'

--- DenseNet/denseNet.py
+++ DenseNet/denseNet.py
@@ -3,6 +3,23 @@ import torch.nn as nn
 
 #! tebgh e mamool ye architecture dige ba main idea e building block:)
 #! ye concept e DenseBlock tarif karde ke be in shekl amal mikone:
+#! idea paper ine ke resNet ro expand kone
+#! yani har laye faghat be khorooji khodesh vasl nabashe
+#! balke be output hame resBlock ha vasle
+#! feature maps from prev layers are concatenated onto the inputs of future layers
+#! hala bottleNeck chie? hamoon conv 1*1 ke bahash depth ro control mikardim
+#! kolan network az 2 bakhsh tashkil shode:
+#! 1 - Dense Blocks ke idea aslish channel re-use hast
+#! 2 - BottleNeck ke az conv + pool tashkhil shode va vazife downSampling dare
+
+#! ye harekat vase architecture haye mokhtalef:
+#! in tedad repeat haye 1*1 -> 3*3 conv too har DenseBlock hast:
+arc = {
+    '121': [6, 12, 24, 16],
+    '169': [6, 12, 32, 32],
+    '201': [6, 12, 48, 32],
+    '264': [6, 12, 64, 48],
+}
 
 class DenseBlock(nn.Module):
 
