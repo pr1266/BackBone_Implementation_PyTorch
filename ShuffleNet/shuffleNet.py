f1d4c581c4283b91f4c610f51249a3b3b6319978
pourya <pourya_1999@yahoo.com>
2022-08-17 21:11:45 +0430

    'some_comments'

--- ShuffleNet/shuffleNet.py
+++ ShuffleNet/shuffleNet.py
@@ -1,14 +1,19 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as functions #for non linear functions
+import torch.nn.functional as functions
 
 class ShuffleBlock(nn.Module):
     def __init__(self, groups):
+        #! here we identify groups count:
         super(ShuffleBlock, self).__init__()
         self.groups = groups
+    
     def forward(self, x):
+        #! here split tensor size into batch_size, channels, height and width:
         N,C,H,W = x.size()
         g = self.groups
+        #! and here we perform shuffle operation:
+        #! indeed, we dont have any weights and computations but a single reshape operation in this block:
         return x.view(N, g, C//g, H, W).permute(0, 2, 1, 3, 4).reshape(N, C, H, W)
 
 class Bottleneck(nn.Module):
@@ -19,7 +24,7 @@ class Bottleneck(nn.Module):
         g = 1 if in_planes == 24 else groups
         self.conv1 = nn.Conv2d(in_planes, mid_planes, kernel_size=1, groups=g, bias=False)
         self.bn1 = nn.BatchNorm2d(mid_planes)
-        self.shuffle1 = ShuffleBlock(groups= g)
+        self.shuffle1 = ShuffleBlock(groups=g)
         self.conv2 = nn.Conv2d(mid_planes, mid_planes, kernel_size=3, stride=stride, padding=1, groups=mid_planes, bias=False)
         self.bn2 = nn.BatchNorm2d(mid_planes)
         self.conv3 = nn.Conv2d(mid_planes, out_planes, kernel_size=1, groups=groups, bias=False)
@@ -27,6 +32,7 @@ class Bottleneck(nn.Module):
         self.shortcut = nn.Sequential()
         if stride==2:
             self.shortcut = nn.Sequential(nn.AvgPool2d(3,stride=2, padding =1))
+    
     def forward(self,x):
         out = functions.relu(self.bn1(self.conv1(x)))
         out = self.shuffle1(out)
