45a35eaa7e5dabcb12ed66b1c015cf7f4f359d6a
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-08-18 02:12:12 +0430

    'shuffle_comments'

--- ShuffleNet/shuffleNet.py
+++ ShuffleNet/shuffleNet.py
@@ -10,7 +10,9 @@ cfg = {
 }
 
 def shuffle(x, groups):
+    #! here we split tensor to batch_size, num_channels, height and width
     N, C, H, W = x.size()
+    #! shuffle operation:
     out = x.view(N, groups, C//groups, H, W).permute(0, 2, 1, 3, 4).contiguous().view(N, C, H, W)
 
     return out
@@ -18,32 +20,37 @@ def shuffle(x, groups):
 class Bottleneck(nn.Module):
     def __init__(self, in_channels, out_channels, stride, groups):
         super().__init__()
-
+        #! as mentioned in paper, first we downsample tensor channels to out_channels/4 as number of middle channels of bottleneck and then upsample it to output channels
         mid_channles = int(out_channels/4)
 
+        #! if in_channels == 24 then we are in stage 2 and use 1 as group because we dont want to use depthwise seperable convolution
         if in_channels == 24:
             self.groups = 1
         else:
             self.groups = groups
 
+        #! first 1X1 conv for down sampling
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels, mid_channles, 1, groups=self.groups, bias=False),
             nn.BatchNorm2d(mid_channles),
             nn.ReLU(inplace=True)
         )
 
+        #! then 3X3 conv for feature extraction
         self.conv2 = nn.Sequential(
             nn.Conv2d(mid_channles, mid_channles, 3, stride=stride, padding=1, groups=mid_channles, bias=False),
             nn.BatchNorm2d(mid_channles),
             nn.ReLU(inplace=True),
         )
+
+        #! finaly 1X1 conv for up sampling to output channels
         self.conv3 = nn.Sequential(
             nn.Conv2d(mid_channles, out_channels, 1, groups=groups, bias=False),
             nn.BatchNorm2d(out_channels)
         )
 
+        #! skip connection:
         self.shortcut = nn.Sequential(nn.AvgPool2d(3, stride=2, padding=1))
-
         self.stride = stride
 
     def forward(self, x):
